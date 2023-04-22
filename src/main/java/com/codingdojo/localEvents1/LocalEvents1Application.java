834c611a52545310668640a2d0e96b2fbd16b4b0
Alyson Rehm <ALY.REHM@gmail.com>
2023-04-21 20:30:29 -0700

    first commit

---
+++ src/main/java/com/codingdojo/localEvents1/LocalEvents1Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.localEvents1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalEvents1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalEvents1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/localEvents1/LocalEvents1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.localEvents1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalEvents1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
